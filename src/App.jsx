4a7758dd6ebb6e88468583f7bf6152e21a94e1a0
Radiat09 <rzsianboyridoy10@gmail.com>
2024-01-13 19:17:59 +0600

    added my-[MD Radiat Hossain Ridoy] button

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,62 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<h1 className="text-8xl">Hexa Scriptors</h1>
-			{/* Add Your Button Here */}
-			<section className="my-52">
-				<button onClick={() =>
-						document.getElementById("my_modal_1").showModal()
-					} className="btn btn-primary text-xl">view info</button>
-			</section>
+  return (
+    <>
+      <h1 className="text-8xl">Hexa Scriptors</h1>
+      {/* Add Your Button Here */}
+      <section className="my-52 flex flex-col gap-5 justify-center items-center w-fit mx-auto">
+        <button
+          onClick={() => document.getElementById("my_modal_1").showModal()}
+          className="btn btn-primary text-xl"
+        >
+          Muztafiz
+        </button>
+        <button
+          onClick={() => document.getElementById("my_modal_2").showModal()}
+          className="btn btn-primary text-xl"
+        >
+          RADIAT RIDOY
+        </button>
+      </section>
 
+      <div>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
 
-
-
-			<div>
-				{/* Open the modal using document.getElementById('ID').showModal() method */}
-			
-				<dialog id="my_modal_1" className="modal">
-					<div className="modal-box">
-						<h3 className="font-bold text-4xl">Md Mustafiz Rahman</h3>
-						<p className="py-4">
-							A student on an intellectual journey, painting the canvas of learning with strokes of curiosity and imagination.
-						</p>
-						<div className="modal-action justify-center">
-							<form className="justify-center" method="dialog">
-								{/* if there is a button in form, it will close the modal */}
-								<button className="btn btn-outline">Close</button>
-							</form>
-						</div>
-					</div>
-				</dialog>
-			</div>
-		</>
-	);
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-4xl">Md Mustafiz Rahman</h3>
+            <p className="py-4">
+              A student on an intellectual journey, painting the canvas of
+              learning with strokes of curiosity and imagination.
+            </p>
+            <div className="modal-action justify-center">
+              <form className="justify-center" method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-outline">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        <dialog id="my_modal_2" className="modal">
+          <div className="modal-box">
+            <h3 className="font-bold text-4xl">MD Radiat Hossain Ridoy</h3>
+            <p className="py-4">
+              A frontend focused Junior Mern-stack Developer.
+            </p>
+            <div className="modal-action justify-center">
+              <form className="justify-center" method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn btn-outline text-3xl">X</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+    </>
+  );
 }
 
 export default App;
