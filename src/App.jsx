8e4e0a6e4f1fe07bea8b54d185cb46f585506f75
Radiat09 <rzsianboyridoy10@gmail.com>
2024-01-15 13:05:57 +0600

    removed un nessesery code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import AboutButton from "./Components/Rean/AboutButton";
 import Myself from "./Components/Ariful/Myself";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="text-8xl">Hexa Scriptors</h1>
